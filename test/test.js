108a0d108e2d2ce597271498378500b010dd8217
Jehy <fate@jehy.ru>
2017-02-08 11:27:11 +0300

    add dummy test

---
+++ test/test.js
@@ -0,0 +1,11 @@
+//var
+//  config = require('../config/config.json');
+
+var envMode = (process.env.NODE_ENV || 'development');
+
+console.log('Running tests in mode ' + envMode);
+describe('Telegram Quest Bot tests', function () {
+  it('should run dummy tests', function (done) {
+    done();
+  });
+});
